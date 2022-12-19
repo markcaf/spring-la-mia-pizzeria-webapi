95151fad9b5971b32433e2b58d1953b980ce6f3f
Marco Cafasso <marcocafassog@gmail.com>
2022-12-19 14:55:54 +0100

    feat: adds pizza ecosystem - pojo, repo and service
    - adds Pizza pojo (id, name, description, price)
    - adds interface PizzaRepo that extends JpaRepository
    - adds save and findAll methods in the service

---
+++ Pizzeria/src/main/java/org/generation/italy/demo/pojo/Pizza.java
@@ -0,0 +1,75 @@
+package org.generation.italy.demo.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table
+public class Pizza {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@NotEmpty(message = "Il nome deve contenere qualcosa")
+	private String name;
+
+	@Column
+	private String description;
+	
+	@NotNull(message = "Il prezzo non può essere vuoto")
+	@Min(value=1, message = "Il prezzo deve essere maggiore di zero")
+	private int price;
+
+	public Pizza() { }
+	public Pizza(String name, String description, int price) {
+		setName(name);
+		setDescription(description);
+		setPrice(price);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return getId() + " - " + getName();
+	}
+
+}
